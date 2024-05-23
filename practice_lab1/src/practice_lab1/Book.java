712ff2d0b34b427daa67b6d2a6cbadd8be5d5b2d
young0519 <kiny0606@naver.com>
2024-05-23 10:40:03 +0900

    Book.java에 주석 달

--- practice_lab1/src/practice_lab1/Book.java
+++ practice_lab1/src/practice_lab1/Book.java
@@ -3,18 +3,19 @@ package practice_lab1;
 public class Book {
 
 	public class Calculator {
+		// 더하기 함수
 		public int add(int a, int b) {
 			return a + b;
 		}
-		
+		// 빼기 함수
 		public int subtract(int a, int b) {
 			return a - b;
 		}
-		
+		// 곱하기 함수
 		public int multiply(int a, int b) {
 			return a * b;
 		}
-		
+		// 나누기 함수
 		public int divide(int a, int b) {
 			if (b == 0) {
 				throw new IllegalArgumentException("Divisor cannot be zero");
